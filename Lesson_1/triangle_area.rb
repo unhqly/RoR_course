f638a7cc4818b19e19218b6d7bfba98394d15d06
Unholy <ya.unholy1@mail.ru>
2022-12-06 23:52:55 +0300

    task with triangle area

---
+++ Lesson_1/triangle_area.rb
@@ -0,0 +1,9 @@
+puts "Enter the base of triangle"
+base = gets.chomp.to_f
+
+puts "Enter the height of triangle"
+height = gets.chomp.to_f
+
+area = base * height / 2
+
+puts "Area of the triangle is #{area}"
