e17c89beafbed33c082bb1c9cf24d40b897a3525
Unholy <ya.unholy1@mail.ru>
2022-12-14 17:58:38 +0300

    fixed leap year problem

--- Lesson_2/dates.rb
+++ Lesson_2/dates.rb
@@ -1,10 +1,10 @@
 month_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-sequence_number = 0
+
 
 puts "Enter the day please"
 day = gets.chomp.to_i
 
-if (day > 31)
+if day > 31 || day <= 0
   puts "Enter the correct day"
   return
 end
@@ -12,7 +12,7 @@ end
 puts "Enter the month please"
 month = gets.chomp.to_i
 
-if (month > 12) || (day > month_days[month-1])
+if month > 12 || month <= 0 || (day > month_days[month-1])
   puts "Enter the correct date"
   return
 end
@@ -24,7 +24,6 @@ if year < 0
   puts "Enter the correct year"
   return
 end
-
 
 def is_leap_year(year)
   if year % 4 == 0
@@ -43,22 +42,15 @@ if !is_leap_year(year) && month == 2 && day > 28
   return
 end
 
-if month == 1
-  sequence_number = day
-  puts "The day has #{sequence_number} sequence number"
-  return
-else
-  for i in 1..month-1 do
-    sequence_number += month_days[i-1]
-  end
-    sequence_number += day
+
+sequence_number = day
+
+for i in 1..month-1 do
+  sequence_number += month_days[i-1]
 end
 
-if is_leap_year(year)
+if is_leap_year(year) || month == 1 || month == 2
   puts "The day has #{sequence_number} sequence number"
 else
   puts "The day has #{sequence_number-1} sequence number"
 end
-
-
-#resolve 28.02.2022 prob
