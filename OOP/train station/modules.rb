d489a40934fb8f8c6cc87684104859cc74dbb5da
Unholy <ya.unholy1@mail.ru>
2023-01-25 23:16:34 +0300

    fixed versions problems v2

---
+++ OOP/train station/modules.rb
@@ -0,0 +1,40 @@
+module CompanyName
+  attr_accessor :company_name
+
+  def get_company_name
+    self.company_name
+  end
+
+  def set_company_name(company_name)
+    self.company_name = company_name
+  end
+end
+
+module InstanceCounter
+  attr_accessor :instances
+  
+  @@instances = 0
+
+  def self.instances
+    @@instances
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    def instances
+      InstanceCounter.instances
+    end
+  end
+
+  protected
+
+  module InstanceMethods
+    def register_instance
+      InstanceCounter.instances += 1
+    end
+  end
+end
