917cdd9f23ad9ef03d75b9f885e54ee36caa32b3
Unholy <ya.unholy1@mail.ru>
2023-02-14 18:21:42 +0300

    edited boxcars' classes

--- OOP/train station/cargo_boxcar.rb
+++ OOP/train station/cargo_boxcar.rb
@@ -1,4 +1,34 @@
 require_relative 'boxcar'
 
 class CargoBoxcar < Boxcar
+
+  attr_accessor :number, :full_volume
+
+  def initialize(number, full_volume)
+    @number = number
+    @full_volume = full_volume
+    @free_volume = full_volume
+    @filled_volume = 0
+    validate!
+  end
+
+  def fill_volume(size)
+    @filled_volume += size
+    @free_volume -= size
+  end
+
+  def available_volume
+    @free_volume
+  end
+
+  def occupied_volume
+    @filled_volume
+  end
+
+  protected
+
+  def validate!
+    raise ArgumentError, "Number is empty" if number.nil? || number == ''
+    raise ArgumentError, "Boxcar's volume is not specified" if full_volume.nil? || full_volume == ''
+  end
 end
--- OOP/train station/passenger_train.rb
+++ OOP/train station/passenger_train.rb
@@ -1,6 +1,7 @@
 require_relative 'train'
 
 class PassengerTrain < Train
+  
   def add_boxcar(boxcar)
     if boxcar.is_a?(PassengerBoxcar)
       super 
