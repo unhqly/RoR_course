ddb4b42f7f4208d20b7f7560ec475ac98fe01800
Unholy <ya.unholy1@mail.ru>
2023-03-17 15:54:07 +0300

    added message

--- OOP/train station/modules.rb
+++ OOP/train station/modules.rb
@@ -22,7 +22,7 @@ module Accessors
     var_name = "@#{attribute}".to_sym
     define_method(attribute) { instance_variable_get(var_name) }
     define_method("#{attribute}=".to_sym) do |value|
-      raise ArgumentError if instance_variable_get(var_name).class != value.class
+      raise ArgumentError, 'Wrong type of value' if instance_variable_get(var_name).class != value.class
       instance_variable_set(var_name, value)
     end
   end
